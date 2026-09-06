32da084c4a55a3744e65d7e8957ba3edd4c16aa8
Vaibhav Jasti <vaibhavjasti@rocky.local>
2026-09-06 18:32:58 +0530

    Add script to precompute the WordEntropyGraph + Update gitignore

---
+++ scripts/build_graph.py
@@ -0,0 +1,19 @@
+import numpy as np
+from wordle_solver import WordEntropyGraph
+
+# Read both files and add words to lists:
+with open('data/valid_guesses.txt', 'r', encoding='utf-8') as file:
+    guesses = file.read().split()
+
+with open('data/valid_answers.txt', 'r', encoding='utf-8') as file:
+    answers = file.read().split()
+
+# Convert to np.ndarray:
+guesses = np.array(guesses)
+answers = np.array(answers)
+
+# Build the Word graph:
+graph = WordEntropyGraph(guesses, answers)
+
+# Save the graph:
+graph.save_graph("graph.npy")
